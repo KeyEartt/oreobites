c040f6e22933e1f9d898a955411fabfe601d3945
KeyEartt <86066659+KeyEartt@users.noreply.github.com>
2026-09-07 23:50:17 +0800

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = 'https://kahhjplojgungfftmsat.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthaGhqcGxvamd1bmdmZnRtc2F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNzc0NTYsImV4cCI6MjEwMzc1MzQ1Nn0.ZUqpI-gkesBlSTc8bxN0jsnI46Xsi3JfCaL7eymdgXI';
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);  
